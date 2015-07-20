1f650d65391a7ea477b5381d4806223363d6fd9b
MarValero <chicemav@gmail.com>
2015-07-20 14:43:38 +0100

    Adding Chapter 9 examples

---
+++ chapter9-exceptions_throw/lib/mathematician.rb
@@ -0,0 +1,19 @@
+#!/urs/bin/ruby
+
+require_relative 'calculator'
+
+class Mathematician
+  def work
+    begin
+      Calculator.new.calculate("£")
+    rescue OperationException => detail
+      "Failed with operation #{detail.operation}"
+    end
+  end
+  def thinking(*subjects)
+    ideas = catch(:done) do
+      ideas = []
+      subjects.each{|subject| ideas << subject and (throw(:done, ideas) if ideas.length > 2)}
+    end
+  end
+end
---
+++ chapter9-exceptions_throw/spec/exception_spec.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/ruby
+
+require "operation_exception"
+
+RSpec.configure do |c|
+  c.color = true
+  c.formatter = 'documentation'
+end
+
+describe OperationException do
+
+  describe ".new" do
+    context "Creating a exception" do
+      it "Option $" do
+        expect(OperationException.new("%").operation).to eql("%")
+      end
+      it "Raise error" do
+        expect{raise OperationException.new("&")}.to raise_error
+      end
+    end
+  end
+end
+
