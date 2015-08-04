c4a72a68791a0a2196fec79f1f316d101bf79fb3
MarValero <chicemav@gmail.com>
2015-08-04 14:26:36 +0100

    Watch server

--- appendix6-grape-watch/lib/api/watch.rb
+++ appendix6-grape-watch/lib/api/watch.rb
@@ -4,46 +4,23 @@ class WatchApi < Grape::API
 
   helpers do
     def get_watch
-      #require 'pry' ; binding.pry
-      Time.now.to_s.match(/(\d\d:\d\d:\d\d)/)
-      @@mutex = Mutex.new
-      @@mutex.synchronize { @@watch ||= (run_watch and $1) }
+      @@watch ||= Time.now
     end
     def set_watch(value)
-      if value.match(/\d\d:\d\d:\d\d/)
-        @@mutex ||= Mutex.new
-        @@th ||= Thread.new{sleep}
-        @@mutex.synchronize {   @@watch=value  and @@th.exit and run_watch}
+      if value.is_a? Time
+        @@watch=value
       else
         raise RuntimeError.new, "Incorrect time"
       end
     end
-    def run_watch
-      @@th = Thread.new do
-        while true
-          sleep(0.5)
-          @@mutex.synchronize do
-            @@watch = @@watch.split(":")
-            if @@watch[2]=="59"
-              @@watch[2]="00"
-              if @@watch[1]=="59"
-                @@watch[1]="00"
-                if @@watch[0] == "23"
-                  @@watch[0] = "00"
-                else
-                  @@watch[0] = (@@watch[0].to_i + 1).to_s
-                end
-              else
-                @@watch[1] = (@@watch[1].to_i+1).to_s
-              end
-            else
-              @@watch[2] = (@@watch[2].to_i + 1).to_s
-            end
-            @@watch = @@watch.collect{|time| time.length==2 ? time : (time<<"0").reverse }
-            @@watch = @@watch.join(":")
-          end
-        end
-      end
+    def get_time(params)
+      year = params["year"] ? params["year"] : Time.now.year
+      month = params["month"] ? params["month"] : Time.now.month
+      day = params["day"] ? params["day"] : Time.now.day
+      hour = params["hour"] ? params["hour"] : Time.now.hour
+      min = params["min"] ? params["min"] : Time.now.min
+      sec = params["sec"] ? params["sec"] : Time.now.sec
+      Time.new(year,month,day,hour,min,sec)
     end
   end
 
@@ -55,28 +32,32 @@ class WatchApi < Grape::API
       if @@num_req != 0
         @@num_req = @@num_req-1
         if @@num_req == 0
-          Time.now.to_s.match(/(\d\d:\d\d:\d\d)/)
-          set_watch($1)
+          set_watch(Time.now)
         end
       end
       answer
     end
 
     params do
-      requires :time, type: String, desc: 'New watch value'
-      optional :num_req, type:Integer, desc: 'Number of request the server will return that anwser'
+      optional :year, type: Integer, desc: 'year'
+      optional :month, type: Integer, desc: 'month'
+      optional :day, type: Integer, desc: 'day'
+      optional :hour, type: Integer, desc: 'hour'
+      optional :min, type: Integer, desc: 'minutes'
+      optional :sec, type: Integer, desc: 'secundes'
+      optional :num_req, type:Integer, desc: 'Number of request the server will return that anwser. 0 means always'
     end
 
     put '/' do
-      @@num_req = (params[:num_req]) ? params[:num_req] : 1
-      status 204
+      require 'json'
       begin
-        set_watch(params[:time])
+        status 204
+        set_watch(get_time(params.to_hash))
+        @@num_req = (params[:num_req]) ? params[:num_req] : 1
       rescue RuntimeError
-         status 400
+        status 400
       end
     end
   end
-
 end
 
--- appendix6-grape-watch/spec/watch_api_spec.rb
+++ appendix6-grape-watch/spec/watch_api_spec.rb
@@ -1,6 +1,4 @@
 require 'api/watch'
-require 'rubygems'
-require File.expand_path('../../lib/api/watch', __FILE__)
 require 'rack/test'
 
 RSpec.configure do |c|
@@ -13,23 +11,23 @@ describe "WatchApi" do
   def app
     WatchApi
   end
+
   describe "API" do
 
     context "Simple test" do
       require 'json'
       it "Get /watch" do
-        put "/watch", {time: "16:00:10"}
         get "/watch"
         expect(last_response.status).to eql(200)
-        expect(JSON.parse(last_response.body)["time"]).to eql("16:00:10")
+        expect(JSON.parse(last_response.body)["time"]).to match(Time.now.to_s)
       end
       it "Put /watch" do
-        put "/watch", {time: "17:24:09"}
+        put "/watch", {hour: 17,min: 24,sec: 9}
         expect(last_response.status).to eql(204)
         expect(last_response.body).to eql("")
       end
       it "Put /watch" do
-        put "/watch", {time: "17:24:13", num_req: 8}
+        put "/watch", {day: "17", num_req: 8}
         expect(last_response.status).to eql(204)
         expect(last_response.body).to eql("")
       end
@@ -38,11 +36,11 @@ describe "WatchApi" do
     context "Bad request" do
       it "Put without time" do
         put "/watch"
-        expect(last_response.status).to eql(400)
-        expect(last_response.body).to eql("time is missing")
+        expect(last_response.status).to eql(204)
+        expect(last_response.body).to eql("")
       end
-      it "Put with bad time" do
-        put "/watch", {time: "bad request"}
+      it "Put with bad hour" do
+        put "/watch", {hour: "bad request"}
         expect(last_response.status).to eql(400)
       end
       it "Put with bad req_time" do
@@ -52,30 +50,58 @@ describe "WatchApi" do
       end
     end
 
-    context "System tests" do
-      it "Put and get" do
-        put "/watch", {time: "16:00:10", num_req: 1}
-        sleep(2)
-        get "/watch"
-        expect(last_response.status).to eql(200)
-        expect(JSON.parse(last_response.body)["time"]).to match(/16:00:1\d/)
-        get "/watch"
-        Time.now.to_s.match(/(\d\d:\d\d:\d\d)/)
-        expect(JSON.parse(last_response.body)["time"]).to eql($1)
-       end
-      it "put 2 request" do
-        put "/watch", {time: "16:00:00", num_req: 2}
-        get "/watch"
-        expect(last_response.status).to eql(200)
-        expect(JSON.parse(last_response.body)["time"]).to match(/16:00:0\d/)
-        get "/watch"
-        expect(last_response.status).to eql(200)
-        expect(JSON.parse(last_response.body)["time"]).to match(/16:00:0\d/)
-        get "/watch"
-        Time.now.to_s.match(/(\d\d:\d\d:\d\d)/)
-        expect(JSON.parse(last_response.body)["time"]).to eql($1)
+    describe "System tests" do
+      context "Put 1 request"do
+        require 'json'
+        it "get 1" do
+          put "/watch", {hour:16, min:0, sec:10, num_req: 1}
+          sleep(2)
+          get "/watch"
+          expect(last_response.status).to eql(200)
+          expect(JSON.parse(last_response.body)["time"]).to match(/16:00:1\d/)
         end
-     end
-
+        it "get 2"do
+          get "/watch"
+          expect(JSON.parse(last_response.body)["time"]).to eql(Time.now.to_s)
+        end
+      end
+      context "put 2 request" do
+        require 'json'
+        it "get 1" do
+          put "/watch", {hour:16, min:0, sec:0, num_req: 2}
+          get "/watch"
+          expect(last_response.status).to eql(200)
+          expect(JSON.parse(last_response.body)["time"]).to match(/16:00:0\d/)
+        end
+        it "get 2" do
+          get "/watch"
+          expect(last_response.status).to eql(200)
+          expect(JSON.parse(last_response.body)["time"]).to match(/16:00:0\d/)
+        end
+        it "get 3" do
+          get "/watch"
+          expect(JSON.parse(last_response.body)["time"]).to eql(Time.now.to_s)
+        end
+      end
+      context "put inf request" do
+        require 'json'
+        it "get 1" do
+          put "/watch", {hour:16, min:0, sec:0, num_req: 0}
+          get "/watch"
+          expect(last_response.status).to eql(200)
+          expect(JSON.parse(last_response.body)["time"]).to match(/16:00:0\d/)
+        end
+        it "get 2" do
+          get "/watch"
+          expect(last_response.status).to eql(200)
+          expect(JSON.parse(last_response.body)["time"]).to match(/16:00:0\d/)
+        end
+        it "get 22" do
+          20.times {get "/watch"}
+          expect(last_response.status).to eql(200)
+          expect(JSON.parse(last_response.body)["time"]).to match(/16:00:0\d/)
+        end
+      end
+    end
   end
 end
